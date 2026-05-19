02b76c6add46e7a31259da8d0aa6a9c030204a5b
nvrsluz-coder <nvrsluz@gmail.com>
2026-05-19 07:59:25 +0500

    Refactor app.py to use environment variables

--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 from flask import Flask
-import requests, os
+import requests
+import os
 
 app = Flask(__name__)
 
 @app.route('/run')
 def run():
+    api_dev_key = os.getenv("API_DEV_KEY")
+    username = os.getenv("PASTEBIN_USERNAME")
+    password = os.getenv("PASTEBIN_PASSWORD")
+    paste_key = os.getenv("PASTE_KEY")
+
     user_key = requests.post("https://pastebin.com/api/api_login.php", data={
-        "api_dev_key": "B1wMYLw7-GZE8rFy_Pkpsjc6l6Pd_mge",
-        "api_user_name": "Ilxom1991",
-        "api_user_password": "Ilxom19910301"
+        "api_dev_key": api_dev_key,
+        "api_user_name": username,
+        "api_user_password": password
     }).text
-    
+
     code = requests.post("https://pastebin.com/api/api_raw.php", data={
-        "api_dev_key": "B1wMYLw7-GZE8rFy_Pkpsjc6l6Pd_mge",
+        "api_dev_key": api_dev_key,
         "api_user_key": user_key,
         "api_option": "show_paste",
-        "api_paste_key": "A9Ms1mU6"
+        "api_paste_key": paste_key
     }).text
-    
+
     return code
 
 if __name__ == '__main__':
